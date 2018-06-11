77a28727f07cd850f96c78494ae2814dd11fc1cd
Romain Mosser <mosser.romain@gmail.com>
2018-06-11 14:30:13 +1000

    dropdown options for the recipes

--- app/models/baking.rb
+++ app/models/baking.rb
@@ -2,4 +2,7 @@ class Baking < ApplicationRecord
   belongs_to :recipe
 
   validates :baking_type, presence: true, length: { maximum: 50 }
+
+  BAKIING_TYPE_LIST = ["Oven", "Pan", "Saucepan", "Pot", "Wok", "Pressure Cooker", "Plancha", "BBQ", "Microwave"]
+  HEAT_UNIT_LIST = ["C°", "F°", "Theromostat"]
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ApplicationRecord
   belongs_to :recipe
 
   validates :ingredient_name, presence: true, length: { maximum: 80 }
+
+  MEASURING_LIST = ["Grams", "Kilograms", "Once", "Pounds", "Centiliters", "Liter", "Spoon", "Handfull", "Tea Cup", "Items"]
 end
