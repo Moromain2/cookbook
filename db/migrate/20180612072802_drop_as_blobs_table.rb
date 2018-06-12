ac16b4b07bafb27bd5c6e12bf3ad87cc1b92725a
Romain Mosser <mosser.romain@gmail.com>
2018-06-12 17:37:20 +1000

    deleted migration file that caused issues for heroku deploy

--- db/migrate/20180612072802_drop_as_blobs_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAsBlobsTable < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :active_storage_blobs
-  end
-
-  def down
-    ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20180612072827_drop_as_attachements_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAsAttachementsTable < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :active_storage_attachments
-  end
-
-  def down
-    ActiveRecord::IrreversibleMigration
-  end
-end
